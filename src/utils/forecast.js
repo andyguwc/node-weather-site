8e4eae626d5109c8471f293efa9cfab30cc86980
Tianyou Gu <tianyou.gu@gmail.com>
2019-06-08 17:14:27 -0700

    changed the forecast text to include humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location'. undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + " It is currently "+ body.currently.temperature + " degrees. "+"There is a " + body.currently.precipProbability + "% to rain")
+            callback(undefined, body.daily.data[0].summary + " It is currently "+ body.currently.temperature + " degrees. "+"There is a " + body.currently.precipProbability + "% to rain. "+"And the humidity is "+body.currently.humidity+".")
         }
     })
 }
